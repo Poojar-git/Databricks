1a3ebb675d7617fe267f649615a9f1208b62ae61
pooja.r@diggibyte.com <pooja.r@diggibyte.com>
2026-07-17 05:10:20 +0000

    Add formatted string literals

---
+++ src/fstring.py
@@ -0,0 +1,11 @@
+#without fstring
+name = "Pooja"
+age = 23
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+
+#with fstring
+name = "Pooja"
+age = 23
+
+print(f"My name is {name} and I am {age} years old.")

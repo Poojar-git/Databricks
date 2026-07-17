ddb154c25ed818bbcf2d63a0b4df7c382f6e7009
pooja.r@diggibyte.com <pooja.r@diggibyte.com>
2026-07-17 04:45:01 +0000

    Add naming convention

---
+++ src/naming_conventions.py
@@ -0,0 +1,9 @@
+#Naming concvention
+Name="Pooja"
+Age = 23
+CompanyName="Diggibyte"
+
+def print_info(): #fuction name-snake_cae
+    print(Name, Age, CompanyName)
+MAX_SALARY=10000 #constant name
+print_info()
